e2ef112b14eab12e667d4008766c8fc363c66793
Muawiyah <muawiyah1705@gmail.com>
2024-06-23 23:38:16 +0300

    First Commit

---
+++ src/TodoItem.jsx
@@ -0,0 +1,11 @@
+export function TodoItem({completed,id,title, toggleTodo, deleteTodo}){
+    return ( <>
+        <li>
+            <label>
+                <input onChange={e=> toggleTodo(id, e.target.checked)} type="checkbox" checked={completed} />
+                {title}
+            </label>
+            <button onClick={()=> deleteTodo(id)} className="btn btn-danger">Delete</button>
+            </li>
+            </>)
+}
